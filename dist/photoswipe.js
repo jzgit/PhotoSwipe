8f2c97cb8af6280e8001afb68c880c4d16a442d9
Jason Zopf <jasonzopf@gmail.com>
2015-09-07 12:47:20 -0400

    Update photoswipe.js

--- dist/photoswipe.js
+++ dist/photoswipe.js
@@ -423,6 +423,9 @@ var _isOpen,
 		return _listeners[name].push(fn);
 	},
 	_shout = function(name) {
+		if (!_listeners) {
+                	return false;
+		}
 		var listeners = _listeners[name];
 
 		if(listeners) {
